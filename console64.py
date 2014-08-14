edce7d76d5269b07d96436ee90c1aafdc6c4275b
Antonio Malara <antonio.malara@booking.com>
2014-08-14 18:30:36 +0200

    providing the simplest input/output hacks i could come up with

--- console64.py
+++ console64.py
@@ -1,4 +1,7 @@
 from py65.devices.mpu6502 import MPU
+from py65.disassembler import Disassembler
+
+import sys
 
 class MemoryRange(object):
     def __init__(self, start, end):
@@ -32,13 +35,17 @@ class Memory(object):
         self.overlay_file('roms/character.rom', 0xd000, 0xdfff)
         self.overlay_file('roms/basic.rom',     0xa000, 0xbfff)
         self.ram = [0x00] * 0x10000
+        self.do_log = False
 
     def __setitem__(self, address, value):
         self.ram[address] = value
-        print '(write) [%04x] = %02x' % (address, value)
+        self.log('\t(write) [%04x] = %02x %s' % (address, value, repr(chr(value))))
 
     def __getitem__(self, address):
         v = None
+
+        if address == 0xd012:
+            return 0
 
         for overlay, data in self.overlays:
             if address in overlay:
@@ -47,7 +54,7 @@ class Memory(object):
         if v is None:
             v = self.ram[address]
 
-        print '(read)  [%04x] = %02x' % (address, v)
+        self.log('\t(read)  [%04x] = %02x' % (address, v))
         return v
 
     def add_overlay(self, data, start, end):
@@ -56,6 +63,10 @@ class Memory(object):
     def overlay_file(self, filename, start, end):
         with open(filename) as f:
             self.add_overlay(f.read(), start, end)
+
+    def log(self, l):
+        if self.do_log:
+            print l
 
 class MemoryCommenter(object):
     def __init__(self, memory):
@@ -73,16 +84,72 @@ class MemoryCommenter(object):
         return v
 
     def comment(self, address):
+        if not self.memory.do_log:
+            return
         for c in self.comments:
             if address in c:
-                print '        %s' % c
+                print '\t        %s' % c
+
+def c64print_(cpu):
+    c = cpu.a
+    num = ord('0') <= c <= ord('9')
+    upp = ord('A') <= c <= ord('Z')
+    low = ord('a') <= c <= ord('z')
+
+    if num or upp or low:
+        s = '(%s)' % chr(c)
+    else:
+        s = ''
+
+    print '%02x %03d' % (c, c), s
+    return
+
+def c64print(cpu):
+    c = cpu.a
+
+    if c == 0x93:
+        pass
+
+    elif c == 0x0a:
+        pass
+
+    elif c == 0x0d:
+        sys.stdout.write('\n')
+
+    else:
+        sys.stdout.write(chr(c))
+
+def c64input(cpu):
+    x = raw_input()
+    y = 0
+
+    for i in x:
+        cpu.memory[0x0200 + y] = ord(i)
+        y += 1
+
+    cpu.memory[0x0200 + y] = 0x0d
+    cpu.x = y
+    cpu.pc = 0xaaca
 
 mem = Memory()
 cpu = MPU()
+dis = Disassembler(cpu)
 
 cpu.memory = MemoryCommenter(mem)
 cpu.pc = mem[0xfffc] + (mem[0xfffd] << 8)
 
-for x in xrange(30):
+for x in xrange(1000000):
+    if cpu.pc == 0xffd2:
+        c64print(cpu)
+
+    if cpu.pc == 0xA560:
+        # mem.do_log = 1
+        c64input(cpu)
+
+    if mem.do_log:
+        mem.do_log = False
+        print '%04X %s' % (cpu.pc, dis.instruction_at(cpu.pc)[1])
+        mem.do_log = True
+
     cpu.step()
 
