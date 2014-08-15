ea91e78cec6ee582a5c1d7a3da6a4d566d44a9ae
Antonio Malara <antonio.malara@booking.com>
2014-08-15 04:57:44 +0200

    the kernal was not enough, (re)introducing more general patching and patch the basic input prompt

--- console64.py
+++ console64.py
@@ -13,7 +13,6 @@ class Memory(object):
         self.kernal = loadfile('roms/kernal.rom')
         self.basic  = loadfile('roms/basic.rom')
         self.ram = [0x00] * 0x10000
-        self.do_log = False
 
     def __setitem__(self, address, value):
         self.ram[address] = value
@@ -30,10 +29,40 @@ class Memory(object):
 
         return self.ram[address]
 
+class Patches(object):
+    def __init__(self):
+        self.patches = {}
+
+        for name, func in vars(self.__class__).iteritems():
+            try:
+                addr = int(name[1:5], 16)
+                self.patches[addr] = func
+            except:
+                pass 
+
+    def __getitem__(self, address):
+        if address in self.patches:
+            return self.patches[address]
+        else:
+            return None
+
+    def xA560_basic_raw_input(c64):
+        line = raw_input()
+
+        for i, char in enumerate(line):
+            c64.cpu.memory[0x0200 + i] = ord(char)
+
+        c64.cpu.memory[0x0200 + len(line)] = 0x00
+        c64.cpu.x = 0xff
+        c64.cpu.y = 0x01
+        c64.cpu.inst_0x60() # RTS
+
 class C64(object):
     def __init__(self):
-        self.mem = Memory()
-        self.cpu = MPU()
+        self.mem     = Memory()
+        self.cpu     = MPU()
+        self.patches = Patches()
+        self.dis     = Disassembler(self.cpu)
 
         pc_low = self.mem[0xfffc]
         pc_hi  = self.mem[0xfffd]
@@ -41,9 +70,13 @@ class C64(object):
         self.cpu.memory = self.mem
         self.cpu.pc     = (pc_hi << 8) + pc_low
 
+        self.trace = False
 
     def step(self):
         pc = self.cpu.pc
+
+        if self.trace:
+            print "0x%04x - %s" % (pc, self.dis.instruction_at(pc)[1])
 
         if kernal.base <= pc and pc <= kernal.end:
             kernal_function = kernal.resolve(pc)
@@ -51,6 +84,11 @@ class C64(object):
                 kernal_function(self)
                 self.cpu.inst_0x60() # RTS
                 return
+
+        patch = self.patches[pc]
+        if patch:
+            patch(self)
+            return
 
         self.cpu.step()
 
